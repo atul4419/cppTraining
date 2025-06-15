2ab5dfb30e920addefe3251d148e6210516af356
Atul Tiwari <tiwari.atul4419@gmail.com>
2025-06-15 12:38:34 +0530

    added the multi threading code snippet

---
+++ MultiThreading/additionOfAllOddAndAllEvenNumer.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<thread>
+#include<chrono>
+#include<atomic>
+using namespace std;
+
+typedef unsigned long long int ulli;
+
+void oddSum(ulli start, const ulli end, ulli &response){
+    while(start <= end){
+        if((start & 1) == 1){
+            response += start;
+        }
+        start++;
+    }
+}
+
+void evenSum(ulli start, const ulli end, ulli &response){
+    while(start <= end){
+        if((start & 1) == 0){
+            response += start;
+        }
+        start++;
+    }
+}
+
+int main(){
+    ulli start = 1, end = 100000000000000000;
+    ulli oddresponse = 0, evenresponse = 0;  // Fixing variable name
+
+    const auto startTime = chrono::system_clock::now();
+
+    // Corrected thread calls
+    thread t1(oddSum, start, end, ref(oddresponse));
+    thread t2(evenSum, start, end, ref(evenresponse));
+    
+    t1.join();
+    t2.join();
+
+    const auto endTime = chrono::system_clock::now();
+    const auto timeTaken = endTime - startTime;
+
+    cout << "Odd Sum: " << oddresponse << endl;
+    cout << "Even Sum: " << evenresponse << endl;
+    cout << "Time taken in sec: " << chrono::duration_cast<chrono::milliseconds>(timeTaken).count() / 1000.0 << endl;
+
+    return 0;
+}
